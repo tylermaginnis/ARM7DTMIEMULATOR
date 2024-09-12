7e8131cad4cd35dd10c5504ab2d241a7b03ac0df
Tyler Maginnis <tylermaginnis@users.noreply.github.com>
2024-09-12 08:54:33 -0400

    Update MOV bin instructions

--- tools/write_instructions.py
+++ tools/write_instructions.py
@@ -1,9 +1,11 @@
 # write_instructions.py
 ARM_MOV_instructions = [
-    0xE1A01002,  # MOV R1, R0, LSL #2
-    0xE3A02001   # MOV R2, #1
+    0xE3B0F001,  # Example MOV instruction: MOV R15, #1 (S bit set)
+    0xE3B01001,  # Example MOV instruction: MOV R1, #1 (S bit set)
+    0xE1A01001,  # Example MOV instruction: MOV R1, R1 (S bit not set)
+    0x03A01001   # Example MOV instruction: MOV R1, #1 (Condition fails)
 ]
 
-with open("../src/test/ARM/MOV/arm7dtmi_test_MOV.bin", "wb") as f:
+with open("../src/test/ARM/MOV/arm7tdmi_test_MOV.bin", "wb") as f:
     for instruction in ARM_MOV_instructions:
         f.write(instruction.to_bytes(4, byteorder='little'))
